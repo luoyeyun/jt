101f06bddd5200720cc2d3dec651d5bedf9ce70a
luoyeyun <13620891608@139.com>
2019-05-26 11:59:42 +0800

    罗业云 修改测试

--- jt-manage/src/main/java/com/jt/manage/service/impl/PicServiceImpl.java
+++ jt-manage/src/main/java/com/jt/manage/service/impl/PicServiceImpl.java
@@ -21,7 +21,7 @@ public class PicServiceImpl implements PicService {
     private String localpath;
     @Value("${image.urlpath}")
     private String urlpath;
-    //test12
+    //test
 
     /**
      * 文件上传实现思路
